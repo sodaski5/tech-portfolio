090b707a376c2df269b4fcfe170a4cd01510340d
sodaski5 <sodaski5@gmail.com>
2023-10-05 03:52:59 +0200

    initail commit

---
+++ README.md
@@ -0,0 +1,2 @@
+// technical portfoil
+// web app built usin react, css, react-bootstrap, animate.css, express js, CORS, nodemailer, react-multi-carouse
---
+++ src/components/Projects.js
@@ -0,0 +1,83 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "X",
+      description: "Social Network Application - Built using React, Next.js, TypeScript, MongoDB",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Neuro Tech",
+      description: "Dynamic Dashboard - Built on React, JavaScript, SQLite",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Vitual Pizza Shop",
+      description: "Online Pizza shop - Designed using Python, Django, PostgreSQL, Heroku",
+      imgUrl: projImg3,
+    }
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>My web applications are a testament to cutting-edge technology integration. Leveraging JavaScript, React, and Next.js for dynamic UI/UX, we've engineered seamless user interactions. On the server-side, Python and Django empower theses platforms with robust functionalities and security. Data management is optimized through a mix of databases - PostgreSQL, SQLite, and MongoDB - balancing scalability and performance. TypeScript, my secret sauce, ensures code reliability and scalability, making my applications a testimony to efficiency and innovation.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} alt=''></img>
+    </section>
+  )
+}
